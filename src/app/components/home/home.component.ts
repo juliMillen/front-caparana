84385dc095e4f0454327508baa4a479520f14d67
Julian Millen <julimillen@gmail.com>
2026-07-01 19:08:48 -0300

    diseñando las distintas secciones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GalleryComponent } from "../gallery/gallery.component";
 import { ReportsComponent } from "../reports/reports.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [GalleryComponent, ReportsComponent],
+  imports: [GalleryComponent, ReportsComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
