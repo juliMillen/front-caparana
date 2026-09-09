650c2e340001395f9ecc9cd1a1add0cd53f5bc13
Julian Millen <julimillen@gmail.com>
2026-09-09 19:54:20 -0300

    comenzando con categorias y jugadores

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -12,6 +12,13 @@ export class PlayerService {
   private apiUrl= environment.apiUrl + '/player'
   constructor(private http:HttpClient) { }
 
+  uploadImage(file:File):Observable<{url:string}>{
+    const formData = new FormData();
+    formData.append('image',file);
+
+    return this.http.post<{url:string}>(`${this.apiUrl}/upload`,formData);
+  }
+
     getPlayers(): Observable<Player[]>{
       return this.http.get<Player[]>(`${this.apiUrl}`);
     }
@@ -20,8 +27,8 @@ export class PlayerService {
       return this.http.get<Player>(`${this.apiUrl}/${idPlayer}`);
     }
   
-    createPlayer(formData:FormData):Observable<Player>{
-      return this.http.post<Player>(`${this.apiUrl}/`,formData);
+    createPlayer(idCategority:number,formData:FormData):Observable<Player>{
+      return this.http.post<Player>(`${this.apiUrl}/create/${idCategority}`,formData);
     }
   
     updatePlayer(idPlayer:number, player:Player):Observable<Player>{
